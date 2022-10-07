70fd5fc3cbc88c6b3bba5324fa8d5b47f74203ae
Santosh Bhosale <santoshbhosale3231@gmail.com>
2022-10-07 17:45:47 +0530

    notes and users frontend

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <Provider store={store}>
     <BrowserRouter>
     <Routes>
       <Route path="/*" element={<App/>} />
     </Routes>
     </BrowserRouter>
+    </Provider>
   </React.StrictMode>
 )
